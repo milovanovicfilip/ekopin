a293df510b8e97810c5cad176f9de9e978537450
vukasincurguz <vukasincurguz@gmail.com>
2026-01-10 14:35:05 +0100

    train initial CNN model for trash bin fullness classification

--- UVRV/main.py
+++ UVRV/main.py
@@ -1,5 +1,66 @@
 import os
-from PIL import Image
-from torchvision import transforms
+import torch
 
-# TODO
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader, random_split
+
+import torch.nn as nn
+import torch.optim as optim
+
+#pot do podatkov
+DATA_DIR = "data-processed"
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+dataset = datasets.ImageFolder(DATA_DIR, transform=transform)
+print("Stevilo slik:", len(dataset))
+
+# train / val split (80/20)
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+
+# model (ResNet18)
+model = models.resnet18(weights="IMAGENET1K_V1")
+model.fc = nn.Linear(model.fc.in_features, 1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+# trening
+EPOCHS = 5
+for epoch in range(EPOCHS):
+    model.train()
+    running_loss = 0.0
+
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.float().unsqueeze(1).to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch+1}/{EPOCHS} - loss: {running_loss:.4f}")
+
+print("Ucenje koncano.")
+
+torch.save(model.state_dict(), "model.pth")
+print("Model shranjen.")
+
+
+
